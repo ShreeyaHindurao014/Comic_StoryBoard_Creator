106393b67e67c6b0b825aa3874f3efcc4314b6c1
ShreeyaHindurao014 <shreeyahindurao@gmail.com>
2026-03-06 23:26:48 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors(
-  {origin:"http://localhost:5173"}
+  //{origin:"http://localhost:5173"}
 ))
 
 app.use(cors());
