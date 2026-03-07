98fa71a2149dd921255e1240707e33bc5203777c
ShreeyaHindurao014 <shreeyahindurao@gmail.com>
2026-03-07 13:05:45 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,32 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "../config/db.js";
 import recordRoutes from "./routes/recordRoutes.js";
-import dns from "node:dns";
+import {connectDB} from "./config/db.js";
+import n from "node:dns/promises";
+import cors from "cors";
 
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-dotenv.config();
-connectDB();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+dotenv.config()
+const app=express()
+const port=process.env.PORT || 3001
 app.use(cors(
-  //{origin:"http://localhost:5173"}
+    {
+        //origin: http://localhost:5173
+    }
 ))
+n.setServers(["1.1.1.1","8.8.8.8"])
+// // app.get('/',(req,res)=>{
+// //     res.status(200).json("hello")
+// //     console.log("hello")
+// // })
+// app.listen(port,()=>{
+//     console.log(http://localhost:${port})
+// })
 
-//app.use(cors());
-app.use(express.json());
+app.use(express.json())
+app.use("/records",recordRoutes)
 
-app.get("/", (req, res) => {
-  res.send("Comic Storyboard Creator Backend Running");
-});
-
-app.use("/api/records", recordRoutes);
-
-app.listen(PORT, () => {
-console.log("Server running successfully");
-console.log(` Base URL: http://localhost:${PORT}`);
-console.log(`Comics API: http://localhost:${PORT}/api/records`);
-
-});
-
-
+connectDB().then(()=>{
+    app.listen(port,()=>{
+        console.log(http://localhost:${port}/records)
+    })
+})
